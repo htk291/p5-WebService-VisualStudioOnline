d44fc943cdc1c442960a2dac917bc2c1c2731641
htk291 <htk291@gmail.com>
2015-03-05 15:40:30 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+WebService::VisualStudioOnline - It's new $module
+
+# SYNOPSIS
+
+    use WebService::VisualStudioOnline;
+
+# DESCRIPTION
+
+WebService::VisualStudioOnline is ...
+
+# LICENSE
+
+Copyright (C) htk291.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+htk291 <htk291@gmail.com>
---
+++ lib/WebService/VisualStudioOnline.pm
@@ -0,0 +1,39 @@
+package WebService::VisualStudioOnline;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WebService::VisualStudioOnline - It's new $module
+
+=head1 SYNOPSIS
+
+    use WebService::VisualStudioOnline;
+
+=head1 DESCRIPTION
+
+WebService::VisualStudioOnline is ...
+
+=head1 LICENSE
+
+Copyright (C) htk291.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+htk291 E<lt>htk291@gmail.comE<gt>
+
+=cut
+
